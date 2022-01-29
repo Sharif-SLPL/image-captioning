e39ec8e41bf575ccb60a081ee0cf68bdcff9ec80
sadrasabouri <sabouri.sadra@gmail.com>
2022-01-29 20:19:50 +0330

    add : 2_train_nic2.py

---
+++ 2_train_nic2.py
@@ -0,0 +1,390 @@
+from numpy import array
+import tensorflow as tf
+from pickle import load
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+# from tensorflow.keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import LSTM, Dense, Lambda, Dot, Activation, Concatenate, Bidirectional
+from keras.layers import Embedding
+from keras.layers import Dropout
+from keras.layers.merge import add
+from keras.callbacks import ModelCheckpoint
+import keras
+import pickle
+import torch
+from keras.layers import Layer
+from keras import backend as K, initializers, regularizers, constraints
+from keras.layers import Layer
+
+
+embedding_matrix = pickle.load(open('embedding_2_3072','rb'))
+
+
+def dot_product(x, kernel):
+    if K.backend() == 'tensorflow':
+        # todo: check that this is correct
+        return K.squeeze(K.dot(x, K.expand_dims(kernel)), axis=-1)
+    else:
+        return K.dot(x, kernel)
+
+
+class Attention3(Layer):
+    def __init__(self,
+                 W_regularizer=None, b_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True,
+                 return_attention=None,
+                 **kwargs):
+
+        self.supports_masking = True
+        self.return_attention = return_attention
+        self.init = initializers.get('glorot_uniform')
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        super(Attention3, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight(shape=(input_shape[-1],),
+                                 initializer=self.init,
+                                 name='w',
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        if self.bias:
+            self.b = self.add_weight(shape=(input_shape[1],),
+                                     initializer='zero',
+                                     name='b',
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+        else:
+            self.b = None
+
+        self.built = True
+
+    def compute_mask(self, input, input_mask=None):
+        # do not pass the mask to the next layers
+        return None
+
+    def call(self, x, mask=None):
+        eij = dot_product(x, self.W)
+
+        if self.bias:
+            eij += self.b
+
+        eij = K.tanh(eij)
+
+        a = K.exp(eij)
+
+        # apply mask after the exp. will be re-normalized next
+        if mask is not None:
+            # Cast the mask to floatX to avoid float64 upcasting in theano
+            a *= K.cast(mask, K.floatx())
+
+        # in some cases especially in the early stages of training the sum may be almost zero
+        # and this results in NaN's. A workaround is to add a very small positive number ε to the sum.
+        # a /= K.cast(K.sum(a, axis=1, keepdims=True), K.floatx())
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        weighted_input = x * K.expand_dims(a)
+
+        result = K.sum(weighted_input, axis=1)
+
+        if self.return_attention:
+            return [result, a]
+        return result
+
+    def compute_output_shape(self, input_shape):
+        if self.return_attention:
+            return [(input_shape[0], input_shape[-1]),
+                    (input_shape[0], input_shape[1])]
+        else:
+            return input_shape[0], input_shape[-1]
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'W_regularizer': self.W_regularizer,
+            'b_regularizer': self.b_regularizer,
+            'W_constraint': self.W_constraint,
+            'b_constraint': self.b_constraint,
+            'bias': self.bias,
+            'return_attention': self.return_attention,
+        })
+        return config
+
+# for id in embedding_matrix.keys():
+#     embedding_matrix[id]=torch.from_numpy(embedding_matrix[id])
+class Attention2(Layer):
+
+    def __init__(self, units=2048, **kwargs):
+        self.units = units
+        super().__init__(**kwargs)
+
+    def __call__(self, inputs):
+        """
+        Many-to-one attention mechanism for Keras.
+        @param inputs: 3D tensor with shape (batch_size, time_steps, input_dim).
+        @return: 2D tensor with shape (batch_size, 128)
+        @author: felixhao28, philipperemy.
+        """
+        hidden_states = inputs
+        hidden_size = int(hidden_states.shape[2])
+        # Inside dense layer
+        #              hidden_states            dot               W            =>           score_first_part
+        # (batch_size, time_steps, hidden_size) dot (hidden_size, hidden_size) => (batch_size, time_steps, hidden_size)
+        # W is the trainable weight matrix of attention Luong's multiplicative style score
+        score_first_part = Dense(hidden_size, use_bias=False, name='attention_score_vec')(hidden_states)
+        #            score_first_part           dot        last_hidden_state     => attention_weights
+        # (batch_size, time_steps, hidden_size) dot   (batch_size, hidden_size)  => (batch_size, time_steps)
+        h_t = Lambda(lambda x: x[:, -1, :], output_shape=(hidden_size,), name='last_hidden_state')(hidden_states)
+        score = Dot(axes=[1, 2], name='attention_score')([h_t, score_first_part])
+        attention_weights = Activation('softmax', name='attention_weight')(score)
+        # (batch_size, time_steps, hidden_size) dot (batch_size, time_steps) => (batch_size, hidden_size)
+        context_vector = Dot(axes=[1, 1], name='context_vector')([hidden_states, attention_weights])
+        pre_activation = Concatenate(name='attention_output')([context_vector, h_t])
+        attention_vector = Dense(self.units, use_bias=False, activation='tanh', name='attention_vector')(pre_activation)
+        return attention_vector
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'vocab_size': self.vocab_size,
+            'num_layers': self.num_layers,
+            'units': self.units,
+            'd_model': self.d_model,
+            'num_heads': self.num_heads,
+            'dropout': self.dropout,
+        })
+        return config
+
+
+
+class attention(Layer):
+    def __init__(self,**kwargs):
+        super(attention,self).__init__(**kwargs)
+
+    def build(self,input_shape):
+        self.W=self.add_weight(name="att_weight",shape=(input_shape[-1],1),initializer="normal")
+        self.b=self.add_weight(name="att_bias",shape=(input_shape[1],1),initializer="zeros")
+        super(attention, self).build(input_shape)
+
+    def call(self,x):
+        et=K.squeeze(K.tanh(K.dot(x,self.W)+self.b),axis=-1)
+        at=K.softmax(et)
+        at=K.expand_dims(at,axis=-1)
+        output=x*at
+        return K.sum(output,axis=1)
+
+    def compute_output_shape(self,input_shape):
+        return (input_shape[0],input_shape[-1])
+
+    def get_config(self):
+        return super(attention,self).get_config()
+
+
+print('embedding loadedd')
+# load doc into memory
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r',encoding='utf-8')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+
+# load a pre-defined list of photo identifiers
+def load_set(filename):
+    doc = load_doc(filename)
+    dataset = list()
+    # process line by line
+    for line in doc.split('\n'):
+        # skip empty lines
+        if len(line) < 1:
+            continue
+        # get the image identifier
+        identifier = line.split('.')[0]
+        dataset.append(identifier)
+    return set(dataset)
+
+
+# load clean descriptions into memory
+def load_clean_descriptions(filename, dataset):
+    # load document
+    doc = load_doc(filename)
+    descriptions = dict()
+    for line in doc.split('\n'):
+        # split line by white space
+        tokens = line.split()
+        # split id from description
+        image_id, image_desc = tokens[0], tokens[1:]
+        # skip images not in the set
+        if image_id in dataset:
+            # create list
+            if image_id not in descriptions:
+                descriptions[image_id] = list()
+            # wrap description in tokens
+            desc = 'startseq ' + ' '.join(image_desc) + ' endseq'
+            # store
+            descriptions[image_id].append(desc)
+    return descriptions
+
+
+# load photo features
+def load_photo_features(filename, dataset):
+    # load all features
+    all_features = load(open(filename, 'rb'))
+    # filter features
+    features = {k: all_features[k] for k in dataset}
+    return features
+
+
+# covert a dictionary of clean descriptions to a list of descriptions
+def to_lines(descriptions):
+    all_desc = list()
+    for key in descriptions.keys():
+        [all_desc.append(d) for d in descriptions[key]]
+    return all_desc
+
+
+# fit a tokenizer given caption descriptions
+def create_tokenizer(descriptions):
+    lines = to_lines(descriptions)
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+
+# calculate the length of the description with the most words
+def max_length(descriptions):
+    lines = to_lines(descriptions)
+    return max(len(d.split()) for d in lines)
+
+
+# create sequences of images, input sequences and output words for an image
+def create_sequences(tokenizer, max_length, descriptions, photos, vocab_size):
+    X1, X2, y = list(), list(), list()
+    # walk through each image identifier
+    for key, desc_list in descriptions.items():
+        # walk through each description for the image
+        for desc in desc_list:
+            # encode the sequence
+            seq = tokenizer.texts_to_sequences([desc])[0]
+            # split one sequence into multiple X,y pairs
+            for i in range(1, len(seq)):
+                # split into input and output pair
+                in_seq, out_seq = seq[:i], seq[i]
+                # pad input sequence
+                in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                # encode output sequence
+                out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+                # store
+                X1.append(photos[key][0])
+                X2.append(in_seq)
+                y.append(out_seq)
+    return array(X1), array(X2), array(y)
+
+
+# define the captioning model
+def define_model(vocab_size, max_length):
+    # feature extractor model
+    inputs1 = Input(shape=(2048,))
+    fe1 = Dropout(0.35)(inputs1)
+    fe2 = Dense(2048, activation='relu')(fe1)
+    # sequence model
+    inputs2 = Input(shape=(max_length,))
+    # se1 = Embedding(vocab_size, 3072,weights=[embedding_matrix]
+    #                 , trainable=False)(inputs2)
+    # se1=Attention3()(se1)
+    se1 = Embedding(vocab_size, 350
+                    , trainable=True)(inputs2)
+    se2 = Dropout(0.35
+                  )(se1)
+    se4 = Bidirectional(LSTM(1024,return_sequences=True),merge_mode='concat')(se2)
+
+    # se4 = Bidirectional(LSTM(1024,return_sequences=True),merge_mode='concat')(se3)
+
+
+
+    # se4 = LSTM(2048,return_sequences=True)(se3)
+    # se6 = Dropout(.2)(se3)
+    # se7= Dense(2048)(se6)
+    se6=Attention3()(se4)
+
+    # se6 = Dropout(.2)(se5)
+    # se7= Dense(2048)(se6)
+    # decoder model
+    decoder1 = add([fe2, se6])
+    decoder2 = Dense(350, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+    # tie it together [image, seq] [word]
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    # summarize model
+    print(model.summary())
+    # plot_model(model, to_file='model.png', show_shapes=True)
+    return model
+
+
+# train dataset
+
+# load training dataset (6K)
+filename = 'Flickr8k_text/train.txt'
+train = load_set(filename)
+print('Dataset: %d' % len(train))
+# descriptions
+train_descriptions = load_clean_descriptions('descriptions.txt', train)
+print('Descriptions: train=%d' % len(train_descriptions))
+# photo features
+train_features = load_photo_features('features.pkl', train)
+print('Photos: train=%d' % len(train_features))
+# prepare tokenizer
+tokenizer = create_tokenizer(train_descriptions)
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+# determine the maximum sequence length
+max_length = max_length(train_descriptions)
+print('Description Length: %d' % max_length)
+# prepare sequences
+X1train, X2train, ytrain = create_sequences(tokenizer, max_length, train_descriptions, train_features, vocab_size)
+
+# dev dataset
+
+# load test set
+filename = 'Flickr8k_text/test.txt'
+test = load_set(filename)
+print('Dataset: %d' % len(test))
+# descriptions
+test_descriptions = load_clean_descriptions('descriptions.txt', test)
+print('Descriptions: test=%d' % len(test_descriptions))
+# photo features
+test_features = load_photo_features('features.pkl', test)
+print('Photos: test=%d' % len(test_features))
+# prepare sequences
+
+print('prepare sequences')
+X1test, X2test, ytest = create_sequences(tokenizer, max_length, test_descriptions, test_features, vocab_size)
+
+# fit model
+
+# define the model
+print('define the model')
+
+model = define_model(vocab_size, max_length)
+# define checkpoint callback
+filepath = 'model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}-attention-final.h5'
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+# fit model
+model.fit([X1train, X2train], ytrain, epochs=6, verbose=1, callbacks=[checkpoint],
+          validation_data=([X1test, X2test], ytest), batch_size=400)
