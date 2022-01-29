fd20fd9a8e12c780101e92fac693ec7ef73eeafb
sadrasabouri <sabouri.sadra@gmail.com>
2022-01-29 20:20:14 +0330

    add : 3_generate.py.

---
+++ 3_generate.py
@@ -0,0 +1,213 @@
+from pickle import load
+from numpy import argmax
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.inception_v3 import preprocess_input
+from keras.models import Model
+from keras.models import load_model
+# from tensorflow.keras.layers import Layer
+from keras import backend as K, initializers, regularizers, constraints
+from keras.layers import Layer
+def dot_product(x, kernel):
+    if K.backend() == 'tensorflow':
+        # todo: check that this is correct
+        return K.squeeze(K.dot(x, K.expand_dims(kernel)), axis=-1)
+    else:
+        return K.dot(x, kernel)
+
+
+class Attention3(Layer):
+    def __init__(self,
+                 W_regularizer=None, b_regularizer=None,
+                 W_constraint=None, b_constraint=None,
+                 bias=True,
+                 return_attention=False,
+                 **kwargs):
+
+        self.supports_masking = True
+        self.return_attention = return_attention
+        self.init = initializers.get('glorot_uniform')
+
+        self.W_regularizer = regularizers.get(W_regularizer)
+        self.b_regularizer = regularizers.get(b_regularizer)
+
+        self.W_constraint = constraints.get(W_constraint)
+        self.b_constraint = constraints.get(b_constraint)
+
+        self.bias = bias
+        super(Attention3, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight(shape=(input_shape[-1],),
+                                 initializer=self.init,
+                                 name='w',
+                                 regularizer=self.W_regularizer,
+                                 constraint=self.W_constraint)
+        if self.bias:
+            self.b = self.add_weight(shape=(input_shape[1],),
+                                     initializer='zero',
+                                     name='b',
+                                     regularizer=self.b_regularizer,
+                                     constraint=self.b_constraint)
+        else:
+            self.b = None
+
+        self.built = True
+
+    def compute_mask(self, input, input_mask=None):
+        # do not pass the mask to the next layers
+        return None
+
+    def call(self, x, mask=None):
+        eij = dot_product(x, self.W)
+
+        if self.bias:
+            eij += self.b
+
+        eij = K.tanh(eij)
+
+        a = K.exp(eij)
+
+        # apply mask after the exp. will be re-normalized next
+        if mask is not None:
+            # Cast the mask to floatX to avoid float64 upcasting in theano
+            a *= K.cast(mask, K.floatx())
+
+        # in some cases especially in the early stages of training the sum may be almost zero
+        # and this results in NaN's. A workaround is to add a very small positive number ε to the sum.
+        # a /= K.cast(K.sum(a, axis=1, keepdims=True), K.floatx())
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        weighted_input = x * K.expand_dims(a)
+
+        result = K.sum(weighted_input, axis=1)
+
+        if self.return_attention:
+            return [result, a]
+        return result
+
+    def compute_output_shape(self, input_shape):
+        if self.return_attention:
+            return [(input_shape[0], input_shape[-1]),
+                    (input_shape[0], input_shape[1])]
+        else:
+            return input_shape[0], input_shape[-1]
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'W_regularizer': self.W_regularizer,
+            'b_regularizer': self.b_regularizer,
+            'W_constraint': self.W_constraint,
+            'b_constraint': self.b_constraint,
+            'bias': self.bias,
+            'return_attention': self.return_attention,
+        })
+        return config
+
+class attention(Layer):
+    def __init__(self,**kwargs):
+        super(attention,self).__init__(**kwargs)
+
+    def build(self,input_shape):
+        self.W=self.add_weight(name="att_weight",shape=(input_shape[-1],1),initializer="normal")
+        self.b=self.add_weight(name="att_bias",shape=(input_shape[1],1),initializer="zeros")
+        super(attention, self).build(input_shape)
+
+    def call(self,x):
+        et=K.squeeze(K.tanh(K.dot(x,self.W)+self.b),axis=-1)
+        at=K.softmax(et)
+        at=K.expand_dims(at,axis=-1)
+        output=x*at
+        return K.sum(output,axis=1)
+
+    def compute_output_shape(self,input_shape):
+        return (input_shape[0],input_shape[-1])
+
+    def get_config(self):
+        return super(attention,self).get_config()
+
+# extract features from each photo in the directory
+def extract_features(filename):
+    # load the model
+    model = InceptionV3()
+    # re-structure the model
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    # load the photo
+    image = load_img(filename, target_size=(299, 299))
+    # convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    # prepare the image for the VGG model
+    image = preprocess_input(image)
+    # get features
+    feature = model.predict(image, verbose=0)
+    return feature
+
+
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+
+# generate a description for an image
+def generate_desc(model, tokenizer, photo, max_length):
+    # seed the generation process
+    in_text = 'startseq'
+    # iterate over the whole length of the sequence
+    for i in range(max_length):
+        # integer encode input sequence
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        # pad input
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        # predict next word
+        yhat = model.predict([photo, sequence], verbose=0)
+        # convert probability to integer
+        yhat = argmax(yhat)
+        # map integer to word
+        word = word_for_id(yhat, tokenizer)
+        # stop if we cannot map the word
+        if word is None:
+            break
+        # append as input for generating the next word
+        in_text += ' ' + word
+        # stop if we predict the end of the sequence
+        if word == 'endseq':
+            break
+    return in_text
+
+
+# load the tokenizer
+tokenizer = load(open('tokenizer.pkl', 'rb'))
+# pre-define the max sequence length (from training)
+max_length = 39
+# load the model
+# model = load_model('model-ep004-loss2.696-val_loss3.117-attention-final.h5')
+
+model = load_model('model.h5',custom_objects={'Attention3': Attention3})
+print(model.summary())
+# load and prepare the photograph
+file_list=['19-Amazing-Music-Games-And-Activities-For-Kids.jpg','25churchsmallgroup.jpg',
+           '20181109_115738.jpg','20190107_150707.jpg',
+           '111766423_4522d36e56.jpg',
+           '133189853_811de6ab2a.jpg','141755292_7a0b3364cf.jpg',
+           '166321294_4a5e68535f.jpg','541063517_35044c554a.jpg','542317719_ed4dd95dc2.jpg',
+           '2238759450_6475641bdb.jpg','2435685480_a79d42e564.jpg',
+           '3223055565_68973f5d20.jpg','3286822339_5535af6b93.jpg',
+           '3437147889_4cf26dd525.jpg','3721799573_2f470950e0.jpg',
+           '3730457171_e66dde8c91.jpg','activities-for-younger-kids_narrow.jpg',
+           ]
+
+for name in file_list:
+    photo = extract_features(name)
+    # generate description
+    description = generate_desc(model, tokenizer, photo, max_length)
+    print(description)
